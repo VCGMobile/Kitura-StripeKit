d2f15c96fbf6f3ba06fe7538c83b7aff739b0925
Ryan VanAlstine <ryan@vcgmobile.com>
2019-07-02 14:02:14 -0500

    Remove Vapor NIOHTTPClient.  Add AsyncHTTPClient

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .package(url: "https://github.com/swift-server/swift-nio-http-client.git",  .branch("master")),
     ],
     targets: [
-        .target(name: "KituraStripeKit", dependencies: ["NIOHTTPClient", "NIOFoundationCompat"], path: "Sources"),
-        .testTarget(name: "KituraStripeKitTests", dependencies: ["NIOHTTPClient", "KituraStripeKit"], path: "Tests")
+        .target(name: "KituraStripeKit", dependencies: ["AsyncHTTPClient", "NIOFoundationCompat"], path: "Sources"),
+        .testTarget(name: "KituraStripeKitTests", dependencies: ["AsyncHTTPClient", "KituraStripeKit"], path: "Tests")
     ]
 )
